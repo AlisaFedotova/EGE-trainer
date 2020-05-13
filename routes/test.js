ed2aec6a8f47f4d65313d69036cfde0d890f7506
Fedotova Alisa <FedotovaAl@outlook.com>
2020-05-13 21:14:22 +0700

    Add Add Task Page Update project structure

--- routes/test.js
+++ routes/test.js
@@ -2,14 +2,33 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {
+    res.render('pages/index', {
         title: 'Home page'
     });
 });
 
 router.get('/create', (req, res) => {
-    res.render('create', {
+    res.render('pages/create', {
         title: 'Create page'
     });
 });
+
+router.get('/addC1', (req, res) => {
+    res.render('pages/addTask/addC1', {
+        title: 'Добавить задание C1'
+    });
+});
+
+router.get('/addC2', (req, res) => {
+    res.render('pages/addTask/addC2', {
+        title: 'Добавить задание C2'
+    });
+});
+
+router.get('/addC4', (req, res) => {
+    res.render('pages/addTask/addC4', {
+        title: 'Добавить задание C4'
+    });
+});
+
 module.exports = router;
